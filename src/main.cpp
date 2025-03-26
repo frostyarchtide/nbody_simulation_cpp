82d1c86f06442bca07578fc3275aaa92ef94615f
Frosty <frosty@archtide.space>
2025-03-25 21:15:17 -0400

    Draw a point

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,21 @@ int main() {
     glfwMakeContextCurrent(window);
 
     debug::print_info((const char*) glGetString(GL_VERSION), "OPENGL VERSION");
+
+    float vertices[] = {
+        0.0f, 0.0f,
+    };
+
+    unsigned int vertex_buffer;
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_POINTS, 0, 1);
 
         glfwSwapBuffers(window);
     }
