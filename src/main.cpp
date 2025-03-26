2c72dbcfc68133ad6fe05b2233b71dc381c16849
Frosty <frosty@archtide.space>
2025-03-25 21:54:49 -0400

    Add imgui and a basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#include <chrono>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <epoxy/gl.h>
 #include <GLFW/glfw3.h>
 
@@ -9,6 +14,9 @@ int main() {
     if (!glfwInit())
         debug::print_error("GLFW failed to initialize", true);
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     GLFWwindow* window = glfwCreateWindow(800, 600, "N-Body Simulation", nullptr, nullptr);
     if (!window) {
@@ -17,7 +25,6 @@ int main() {
     }
 
     glfwMakeContextCurrent(window);
-
     debug::print_info((const char*) glGetString(GL_VERSION), "OPENGL VERSION");
 
     float vertices[] = {
@@ -28,16 +35,59 @@ int main() {
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    io.IniFilename = NULL;
+    io.LogFilename = NULL;
+
+    bool drawPoint = true;
+
+    std::chrono::high_resolution_clock::time_point last_time = std::chrono::high_resolution_clock::now();
     
     while (!glfwWindowShouldClose(window)) {
+        std::chrono::high_resolution_clock::time_point current_time = std::chrono::high_resolution_clock::now();
+        float delta_time = std::chrono::duration_cast<std::chrono::duration<float>>(current_time - last_time).count();
+
         glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_POINTS, 0, 1);
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (drawPoint)
+            glDrawArrays(GL_POINTS, 0, 1);
+
+        ImGui::SetNextWindowPos({ 0, 0 });
+        ImGui::SetNextWindowSize({ 0, 0 });
+        ImGuiWindowFlags flags =
+            ImGuiWindowFlags_NoResize
+            | ImGuiWindowFlags_NoTitleBar;
+
+        ImGui::Begin("Debug", NULL, flags);
+        ImGui::Text("%d", (unsigned int) (1.0f / delta_time + 0.5f));
+        ImGui::Checkbox("Draw Point", &drawPoint);
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
+
+        last_time = current_time;
     }
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glDeleteBuffers(1, &vertex_buffer);
     glfwDestroyWindow(window);
     glfwTerminate();
 }
