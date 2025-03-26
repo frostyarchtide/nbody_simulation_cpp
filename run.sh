bc848ba40940dbf30f1c715499716c1fe63e43df
Frosty <frosty@archtide.space>
2025-03-26 11:51:51 -0400

    Add configure command to run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,12 @@ if [ ! -d "build" ]; then
     meson setup build --buildtype=debugoptimized
 fi
 
-meson compile -C build
-build/nbody_simulation
+if [ "$1" == "configure" ]; then
+    if [ -z "$2" ]; then
+        meson configure build -Dglfw:display-api=auto
+    else
+        meson configure build -Dglfw:display-api=$2
+    fi
+fi
+
+meson compile -C build && build/nbody_simulation
